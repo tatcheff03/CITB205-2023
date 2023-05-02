ed4c7844ca543c38d1d3c4d6bed874081b97c424
tatcheff03 <a.tatcheff@abv.bg>
2023-05-02 10:35:00 +0300

    changes to lab10

---
+++ lab10/window.cpp
@@ -0,0 +1,6 @@
+#include "window.h"
+
+MainWindow::MainWindow()
+{
+setFixedSize(480,320);
+}
--- lab10/window.h
+++ lab10/window.h
@@ -1,7 +1,10 @@
+#pragma once
 #include <QWidget>
 
 class MainWindow:public QWidget
 {
-
+    Q_OBJECT
+    public:
+    MainWindow();
     
 };
