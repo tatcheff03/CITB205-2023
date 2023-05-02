a9cc57f34459ef2a963e0c8cf625a6772fae222b
tatcheff03 <a.tatcheff@abv.bg>
2023-05-02 11:18:16 +0300

    qticker

--- lab10/window.cpp
+++ lab10/window.cpp
@@ -1,10 +1,20 @@
 #include "window.h"
+#include <QPainter>
+MainWindow::MainWindow(): sq (0,0,20,20),dir(20,0){
 
-MainWindow::MainWindow()
-{
+
     setFixedSize(480, 320);
+    ticker.start(1000);
+    connect(&ticker,&QTimer::timeout,this,&MainWindow::onTick);
 }
 void MainWindow::paintEvent(QPaintEvent *event)
 {
-
+QPainter p(this);
+p.setBrush(Qt::black);
+p.setPen(Qt::red);
+p.drawRect(sq);
+}
+void MainWindow::onTick(){
+sq.translate(dir);
+update();
 }
--- lab10/window.h
+++ lab10/window.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <QWidget>
-
+#include <QTimer>
+#include <QEvent>
 class MainWindow : public QWidget
 {
 Q_OBJECT
@@ -8,5 +9,15 @@ public:
     MainWindow();
  protected:
  void paintEvent(QPaintEvent *event) override;
+
+ private slots:
+ void onTick();
+
+ 
+ private:
+ QTimer ticker;
+ QRect sq;
+ QPoint dir;
+
 
 };
